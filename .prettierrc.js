45a45ae5b342974b0c3cc0661a0db3b2f01d9be4
kimseonggon <shuw75@gmail.com>
2025-06-30 14:55:08 +0900

    prettier 일괄 적용

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'auto',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  insertPragma: false,
+  trailingComma: 'none'
+}
